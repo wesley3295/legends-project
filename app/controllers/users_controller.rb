25ee22661a3584dc603903b2fce6fdc61df51710
Doode1524 <Doode1524@gmail.com>
2020-12-26 22:50:27 -0500

    create signup form and set up routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+    get '/signup' do
+        erb :'users/signup'
+    end
+
+    post '/signup' do
+        user = User.new(params[:user])
+        if user.save
+            session[:user_id] = user.id
+            redirect "/games"
+        else
+            redirect "/signup"
+        end
+    end
+        
 end

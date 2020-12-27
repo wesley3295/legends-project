45a82f232376bda13749a14d37e7cff923af2c4f
Doode1524 <Doode1524@gmail.com>
2020-12-26 19:08:26 -0500

    create new team routing

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,22 @@
 class TeamsController < ApplicationController
-    get '/team/new' do
+    get '/teams/new' do
         erb :'/teams/new'
+    end
+    
+    
+    get '/teams' do
+        @teams = Team.all
+        erb :'/teams/index'
+    end
+
+    post '/teams' do
+        team = Team.new(params)
+        team.save
+        if team.save
+            redirect '/teams'
+        else
+            redirect '/teams/new'
+        end
+        
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201226222321) do
+ActiveRecord::Schema.define(version: 20201226232113) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "team_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"

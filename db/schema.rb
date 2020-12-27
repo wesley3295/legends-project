d2bfb92d71cfbfa3a65754a284ec9423affa63b6
Doode1524 <Doode1524@gmail.com>
2020-12-26 21:54:43 -0500

    add user id to teams table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201227024915) do
+ActiveRecord::Schema.define(version: 20201227025353) do
 
   create_table "teams", force: :cascade do |t|
     t.string   "team_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|

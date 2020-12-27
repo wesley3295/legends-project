74acc0c01050a997fc0f18d827e54821da0ae8d5
Doode1524 <Doode1524@gmail.com>
2020-12-26 21:51:45 -0500

    update users table, password_digest column

---
+++ db/migrate/20201227024915_change_users_column.rb
@@ -0,0 +1,5 @@
+class ChangeUsersColumn < ActiveRecord::Migration
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201226232113) do
+ActiveRecord::Schema.define(version: 20201227024915) do
 
   create_table "teams", force: :cascade do |t|
     t.string   "team_name"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 20201226232113) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end

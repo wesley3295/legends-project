f8db6851bee9773ec4303da24c6bc7b055a7c0a2
Doode1524 <Doode1524@gmail.com>
2020-12-26 22:57:50 -0500

    create logged in user session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,14 @@ class ApplicationController < Sinatra::Base
     set :session_secret, ENV['SESSION_SECRET']
   end
 
+  helpers :logged_in?
+
   get "/" do
     erb :welcome
+  end
+
+  def logged_in?
+    !!session[:user_id]
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         user = User.new(params[:user])
         if user.save
             session[:user_id] = user.id
-            redirect "/games"
+            redirect "/teams"
         else
             redirect "/signup"
         end

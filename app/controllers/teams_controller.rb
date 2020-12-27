5f87e26d843538e461809f7a2b5379fcbfa449c5
Doode1524 <Doode1524@gmail.com>
2020-12-26 20:16:50 -0500

    create edit and delete routes and functionality

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,8 +3,13 @@ class TeamsController < ApplicationController
         erb :'/teams/new'
     end
 
+    get '/teams/:id/edit' do
+        find_team
+        erb :'teams/edit'
+    end
+
     get '/teams/:id' do
-        @team = Team.find_by_id(params[:id])
+        find_team
         erb :'/teams/show'
       end
     
@@ -20,6 +25,28 @@ class TeamsController < ApplicationController
         else
             redirect '/teams/new'
         end
-        
     end
+
+    patch '/teams/:id' do
+        find_team
+        if @team.update(params[:team_name])
+            redirect "/teams/#{@team.id}"
+        else
+            redirect "/teams/#{@team.id}/edit"
+        end
+    end
+
+    delete '/teams/:id' do
+        find_team
+        if @team
+            @team.destroy
+            redirect "/teams"
+        end
+    end
+
+    private
+    def find_team
+        @team = Team.find_by_id(params[:id])
+    end 
+
 end
